5741d1737c2baff79e45c18ea40881101f3a588a
Dmitry <usk-dima2@yandex.ru>
2016-06-14 16:32:45 +0300

    реализована корректная отрисовка деревьев. Теперь герои не ходят по листьям деревьев, а скрываются за ними

--- src/main/java/ru/game/pattern/model/staticObjects/MediumTree.java
+++ src/main/java/ru/game/pattern/model/staticObjects/MediumTree.java
@@ -2,7 +2,10 @@ package ru.game.pattern.model.staticObjects;
 
 import ru.game.pattern.controller.Property;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -13,13 +16,23 @@ public class MediumTree extends StaticPhysicalGameObject {
 
     private static final String IMAGE_PATH = Property.RESOURSES_PATH + "static/tree_medium.png";
 
+    private static final String LEAVES_IMAGE_PATH = Property.RESOURSES_PATH + "static/tree_medium_leaves.png";
+
     private static final int TERRITORY_RADIUS = 15;
 
     private static final Point IMAGE_SIZE = new Point(51, 89);
 
+    private BufferedImage leavesImage;
+
     public MediumTree(Point location) throws IOException{
         super(location, TERRITORY_RADIUS, IMAGE_PATH, IMAGE_SIZE);
+        leavesImage = ImageIO.read(new File(LEAVES_IMAGE_PATH));
         printTerritoryRadius = true;
         additionalIamgeShift = -20;
+    }
+
+    @Override
+    public void drawSpecialAfterAll(Graphics2D g) {
+        g.drawImage(leavesImage, location.x - IMAGE_SIZE.x / 2, location.y - IMAGE_SIZE.y / 2 + additionalIamgeShift, null);
     }
 }
